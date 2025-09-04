ccd100e0e4144713ef6286c56750707aef57f5d0
s4rt4 <80631216+s4rt4@users.noreply.github.com>
2025-09-04 10:09:55 +0700

    Add files via upload

---
+++ stations2.js
@@ -0,0 +1,5 @@
+const stationsSource2 = [
+  { name: "Prambors FM", url: "https://23683.live.streamtheworld.com/PRAMBORS_FM_SC" },
+  { name: "Hard Rock FM", url: "https://n08.radiojar.com/7csmg90fuqruv.mp3" },
+  { name: "I-Radio Jakarta", url: "https://n08.radiojar.com/4ywdgup3bnzuv" }
+];
